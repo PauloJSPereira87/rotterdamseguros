eb5a6a1395c83c429255aaf8ad521bf4ec6b71f2
PauloJSPereira87 <paulojspereira87@gmail.com>
2025-12-19 17:04:19 -0300

    Delete rotterdamseguros-completo directory

--- rotterdamseguros-completo/rotterdamseguros-api/src/main/java/com/rotterdamseguros/repository/LeadRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rotterdamseguros.repository;
-
-import com.rotterdamseguros.model.Lead;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LeadRepository extends JpaRepository<Lead, Long> {
-}
